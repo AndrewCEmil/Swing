52f09fcd7f998c4a0e43285872f03e0d01b2d389
ace <andrew.emil@gmail.com>
2017-08-17 17:44:30 -0400

    Support for attaching the arrow

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Grappler grappler;
+	bool attached;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		grappler = GameObject.Find ("Player").GetComponent<Grappler> ();
+		attached = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log ("collided");
+		GameObject anchor = collision.collider.gameObject;
+		if (!attached && anchor.tag == "Anchor") {
+			AttachToAnchor (anchor);
+			attached = true;
+		}
+	}
+
+	void AttachToAnchor(GameObject anchor) {
+		rb.velocity = new Vector3 (0, 0, 0);
+		grappler.AttachArrow (anchor);
+	}
+}
--- Assets/Scripts/Grappler.cs
+++ Assets/Scripts/Grappler.cs
@@ -6,11 +6,14 @@ public class Grappler : MonoBehaviour {
 
 	private GameObject arrow;
 	private GameObject player;
+	private Vector3 connectedAnchorPosition;
+
 	// Use this for initialization
 	void Start () {
 		arrow = GameObject.Find ("Arrow");
 		arrow.SetActive (false);
 		player = GameObject.Find ("Player");
+		connectedAnchorPosition = new Vector3 (0, -0.5f, 0);
 	}
 	
 	// Update is called once per frame
@@ -22,6 +25,15 @@ public class Grappler : MonoBehaviour {
 	public void Shoot(GameObject anchor) {
 		ShootArrow(anchor);
 		//2) Set mode to shooting
+	}
+
+	public void AttachArrow (GameObject anchor) {
+		arrow.AddComponent<HingeJoint> ();
+		HingeJoint hinge = arrow.GetComponent<HingeJoint> ();
+		hinge.autoConfigureConnectedAnchor = false;
+		hinge.connectedAnchor = connectedAnchorPosition;
+		hinge.connectedBody = anchor.GetComponent<Rigidbody> ();
+		hinge.enableCollision = true;
 	}
 
 	void ShootArrow(GameObject anchor) {
