e6db1a23ade75dd74ee1cb258f9e37e57c47b590
ace <andrew.emil@gmail.com>
2017-08-26 12:59:10 -0400

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (0, 0, -50);
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+		transform.LookAt (player.transform.position);
+	}
+}
