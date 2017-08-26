f36cf91900900ef93565495068da847584e8ae6c
ace <andrew.emil@gmail.com>
2017-08-26 12:15:18 -0400

    Threw away what I had, time for new swinging

--- Assets/Scripts/Grappler.cs
+++ Assets/Scripts/Grappler.cs
@@ -6,43 +6,34 @@ public enum GrapplerMode
 {
 	Off,
 	Shooting,
-	Retracting,
 	Attached
 }
 
 public class Grappler : MonoBehaviour {
 
 	private GameObject arrow;
+	private Rigidbody arrowRb;
 	private ArrowController arrowController;
 	private GameObject player;
-	private GameObject baseLink;
 	private GameObject plane;
 	private Vector3 anchorPosition;
 	private Vector3 axis;
 	private Vector3 secondaryAxis;
 	private GrapplerMode mode;
-	private GameObject lastLink;
-	private float linkDistance;
-	private List<GameObject> links;
-	private float timePerRetraction;
-	private float timeSinceLastRetraction;
+	private Vector3 jointConnectedAnchor;
 
 	// Use this for initialization
 	void Start () {
 		arrow = GameObject.Find ("Arrow");
+		arrowRb = arrow.GetComponent<Rigidbody> ();
 		arrowController = arrow.GetComponent<ArrowController> ();
 		arrow.SetActive (false);
 		player = GameObject.Find ("Player");
-		baseLink = GameObject.Find ("BaseLink");
 		plane = GameObject.Find ("Plane");
 		anchorPosition = new Vector3 (0, 0, .75f);
 		axis = new Vector3 (1f, 0, 0);
 		secondaryAxis = new Vector3 (0, 1f, 0);
 		mode = GrapplerMode.Off;
-		linkDistance = 2f;
-		links = new List<GameObject> ();
-		timePerRetraction = .02f;
-		timeSinceLastRetraction = 0;
 	}
 	
 	// Update is called once per frame
@@ -51,10 +42,6 @@ public class Grappler : MonoBehaviour {
 		case GrapplerMode.Off:
 			break; //noop
 		case GrapplerMode.Shooting:
-			DoShooting ();
-			break;
-		case GrapplerMode.Retracting:
-			Retract ();
 			break;
 		case GrapplerMode.Attached:
 			break; //noop
@@ -65,10 +52,9 @@ public class Grappler : MonoBehaviour {
 	public void Shoot(GameObject anchor) {
 		//TODO this section gets removed once we switch to vr
 		if (mode == GrapplerMode.Attached) {
-			StartRetraction ();
+			BreakLink ();
 		} else if (mode == GrapplerMode.Off) {
 			ShootArrow (anchor);
-			lastLink = arrow;
 			mode = GrapplerMode.Shooting;
 		} else {
 			//Aready shooting or retracting, do nothing
@@ -76,84 +62,31 @@ public class Grappler : MonoBehaviour {
 	}
 
 	 
-	public void StartRetraction() {
+	public void BreakLink() {
 		Destroy (arrow.GetComponent<ConfigurableJoint> ());
 		arrowController.attached = false;
 		arrow.SetActive (false);
-		timeSinceLastRetraction = 0f;
-		mode = GrapplerMode.Retracting;
-	}
-
-	public void AttachArrow (GameObject anchor) {
-		arrow.transform.LookAt (anchor.transform.position);
-		arrow.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
-
-		BuildJoint (arrow, anchor);
-
-		FinishLinks ();
-		mode = GrapplerMode.Attached;
-	}
-
-	private void DoShooting() {
-		AddLinks ();
-	}
-
-	private void Retract() {
-		timeSinceLastRetraction += Time.deltaTime;
-		int numToRemove = (int) Mathf.Floor (Mathf.Min (links.Count, timeSinceLastRetraction / timePerRetraction));
-		for (int i = 0; i < numToRemove; i++) {
-			Destroy (links [i]);
-			links.RemoveAt (i);
-		}
-		timeSinceLastRetraction = timeSinceLastRetraction % timePerRetraction;
-		if (links.Count == 0) {
-			TriggerRetracted ();
-		}
-	}
-
-	private void TriggerRetracted() {
-		//TODO anything else?
 		mode = GrapplerMode.Off;
 	}
 
-	void FinishLinks() {
-		AddLinks ();
-		LinkPlayer ();
-	}
+	//TODO need to attach where it _hits_
+	public void AttachArrow (GameObject anchor) {
+		arrow.transform.LookAt (anchor.transform.position);
+		arrow.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
+		arrowRb.useGravity = false;
+		arrowRb.isKinematic = true;
 
-	void AddLinks() {
-		float distance = Vector3.Distance(player.transform.position, lastLink.transform.position);
-		while(distance > linkDistance) {
-			AddLink();
-			distance = Vector3.Distance(player.transform.position, lastLink.transform.position);
-		}
-	}
+		//TODO right here
+		BuildJoint (player, arrow);
 
-	void AddLink() {
-		GameObject link = Instantiate (baseLink);
-		Vector3 direction = (lastLink.transform.position - player.transform.position).normalized;
-		Vector3 newLinkPos = lastLink.transform.position - direction * 1.5f;
-		link.transform.position = newLinkPos;
-		link.transform.LookAt (lastLink.transform.position);
-
-		BuildJoint (link, lastLink);
-		links.Add (link);
-
-		lastLink = link;
-	}
-
-	void LinkPlayer() {
-		player.transform.LookAt (lastLink.transform.position);
-
-		BuildJoint (player, lastLink);
-
-		lastLink = player;
+		mode = GrapplerMode.Attached;
 	}
 
 	void BuildJoint(GameObject link, GameObject anchor) {
 		link.AddComponent<ConfigurableJoint> ();
 		ConfigurableJoint joint = link.GetComponent<ConfigurableJoint> ();
-		joint.autoConfigureConnectedAnchor = true;
+		joint.autoConfigureConnectedAnchor = false;
+		joint.connectedAnchor = jointConnectedAnchor;
 		joint.axis = axis;
 		joint.secondaryAxis = secondaryAxis;
 		joint.anchor = anchorPosition;
@@ -168,7 +101,7 @@ public class Grappler : MonoBehaviour {
 		spring.damper = 0;
 		joint.linearLimitSpring = spring;
 		SoftJointLimit limit = new SoftJointLimit ();
-		limit.limit = .1f;
+		limit.limit = Vector3.Distance (player.transform.position, anchor.transform.position);
 		limit.bounciness = 0f;
 		limit.contactDistance = .1f;
 		joint.linearLimit = limit;
@@ -178,12 +111,13 @@ public class Grappler : MonoBehaviour {
 
 	void ShootArrow(GameObject anchor) {
 		arrow.SetActive (true);
+		arrowRb.useGravity = true;
+		arrowRb.isKinematic = false;
 		arrow.transform.LookAt (anchor.transform.position);
 		arrow.transform.position = player.transform.position;
 		arrow.transform.LookAt (anchor.transform.position);
 		Vector3 direction = (anchor.transform.position - player.transform.position).normalized;
 
-		Rigidbody arrowRb = arrow.GetComponent<Rigidbody> ();
 		arrowRb.AddForce(direction * 40000);
 		Physics.IgnoreCollision(arrow.GetComponent<Collider>(), player.GetComponent<Collider>());
 	}
