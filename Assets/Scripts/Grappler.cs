c259d8be846553b029e63782ca3c87bf36e11cb0
ace <andrew.emil@gmail.com>
2017-10-04 00:39:28 -0400

    Support for raycasting partially in place, got some bugs, going to bed

--- Assets/Scripts/Grappler.cs
+++ Assets/Scripts/Grappler.cs
@@ -11,33 +11,25 @@ public enum GrapplerMode
 
 public class Grappler : MonoBehaviour {
 
-	private GameObject arrow;
-	private Rigidbody arrowRb;
-	private ArrowController arrowController;
 	private GameObject player;
 	private GameObject plane;
 	private Vector3 anchorPosition;
 	private Vector3 axis;
 	private Vector3 secondaryAxis;
 	private GrapplerMode mode;
-	private Vector3 jointConnectedAnchor;
 	private LineRenderer lineRenderer;
-	private float maxArrowDistance;
+	private float maxRayDistance;
 	private bool isRetracting;
 	private float springRetractionScale;
 
 	// Use this for initialization
 	void Start () {
-		arrow = GameObject.Find ("Arrow");
-		arrowRb = arrow.GetComponent<Rigidbody> ();
-		arrowController = arrow.GetComponent<ArrowController> ();
-		arrow.SetActive (false);
 		player = GameObject.Find ("Player");
 		anchorPosition = new Vector3 (0, 0, .75f);
 		axis = new Vector3 (1f, 0, 0);
 		secondaryAxis = new Vector3 (0, 1f, 0);
 		mode = GrapplerMode.Off;
-		maxArrowDistance = 50f;
+		maxRayDistance = 50f;
 		isRetracting = false;
 		InitializeLine ();
 		springRetractionScale = 10f;
@@ -55,7 +47,6 @@ public class Grappler : MonoBehaviour {
 		case GrapplerMode.Off:
 			break; //noop
 		case GrapplerMode.Shooting:
-			DoShooting ();
 			DoLine ();
 			break;
 		case GrapplerMode.Attached:
@@ -101,7 +92,7 @@ public class Grappler : MonoBehaviour {
 		if (mode == GrapplerMode.Attached) {
 			BreakLink ();
 		} else if (mode == GrapplerMode.Off) {
-			ShootArrow (anchor);
+			ShootRay (anchor);
 			mode = GrapplerMode.Shooting;
 		} else {
 			//Aready shooting or retracting, do nothing
@@ -110,37 +101,29 @@ public class Grappler : MonoBehaviour {
 
 	 
 	public void BreakLink() {
-		Destroy (arrow.GetComponent<ConfigurableJoint> ());
-		arrowController.attached = false;
-		arrow.SetActive (false);
 		mode = GrapplerMode.Off;
 		lineRenderer.positionCount = 0;
 	}
 
 	//TODO need to attach where it _hits_
-	public void AttachArrow (GameObject anchor) {
-		arrow.transform.LookAt (anchor.transform.position);
-		arrow.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
-		arrowRb.useGravity = false;
-		arrowRb.isKinematic = true;
+	public void Attach (GameObject anchor) {
+		BuildJoint (player, anchor);
+		CreateLine (anchor);
 
-		BuildJoint (player, arrow);
-
+		anchorPosition = anchor.transform.position;
 		mode = GrapplerMode.Attached;
 	}
 
-	private void DoShooting() {
-		if (Vector3.Distance (arrow.transform.position, player.transform.position) > maxArrowDistance) {
-			arrow.SetActive (false);
-			mode = GrapplerMode.Off;
-			lineRenderer.positionCount = 0;
-		}
+	private void CreateLine(GameObject anchor) {
+		lineRenderer.positionCount = 2;
+		lineRenderer.SetPosition (0, player.transform.position);
+		lineRenderer.SetPosition (1, anchor.transform.position);
 	}
 
 	private void DoLine() {
 		if (lineRenderer.positionCount > 0) {
 			lineRenderer.SetPosition (0, player.transform.position);
-			lineRenderer.SetPosition (1, arrow.transform.position);
+			lineRenderer.SetPosition (1, anchorPosition);
 		}
 	}
 
@@ -148,7 +131,6 @@ public class Grappler : MonoBehaviour {
 		link.AddComponent<ConfigurableJoint> ();
 		ConfigurableJoint joint = link.GetComponent<ConfigurableJoint> ();
 		joint.autoConfigureConnectedAnchor = false;
-		joint.connectedAnchor = jointConnectedAnchor;
 		joint.axis = axis;
 		joint.secondaryAxis = secondaryAxis;
 		joint.anchor = anchorPosition;
@@ -163,29 +145,22 @@ public class Grappler : MonoBehaviour {
 		spring.damper = 0;
 		joint.linearLimitSpring = spring;
 		SoftJointLimit limit = new SoftJointLimit ();
-		limit.limit = Vector3.Distance (player.transform.position, anchor.transform.position) + .5f;
+		limit.limit = Vector3.Distance (link.transform.position, anchor.transform.position) + .5f;
 		limit.bounciness = 0f;
 		limit.contactDistance = .1f;
 		joint.linearLimit = limit;
 		joint.connectedBody = anchor.GetComponent<Rigidbody> ();
-		joint.projectionMode = JointProjectionMode.PositionAndRotation;
+		//joint.projectionMode = JointProjectionMode.PositionAndRotation;
 	}
 
-	void ShootArrow(GameObject anchor) {
-		arrow.SetActive (true);
-		arrowRb.useGravity = true;
-		arrowRb.isKinematic = false;
-		arrowRb.velocity = new Vector3 (0, 0, 0);
-		arrow.transform.LookAt (anchor.transform.position);
-		arrow.transform.position = player.transform.position;
-		arrow.transform.LookAt (anchor.transform.position);
+	void ShootRay(GameObject anchor) {
 		Vector3 direction = (anchor.transform.position - player.transform.position).normalized;
-
-		arrowRb.AddForce(direction * 3000);
-		Physics.IgnoreCollision(arrow.GetComponent<Collider>(), player.GetComponent<Collider>());
-
-		lineRenderer.positionCount = 2;
-		lineRenderer.SetPosition (0, player.transform.position);
-		lineRenderer.SetPosition (1, arrow.transform.position);
+		RaycastHit hit;
+		bool didHit = Physics.Raycast (player.transform.position, direction, out hit, maxRayDistance);
+		if (didHit && hit.collider.gameObject.CompareTag ("Anchor")) {
+			Attach (hit.collider.gameObject);
+		} else {
+			//TODO miss
+		}
 	}
 }
