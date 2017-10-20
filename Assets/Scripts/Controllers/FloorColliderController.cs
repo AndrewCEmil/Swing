91e52ff45989a029022762fcad3e3297d64dc6e0
ace <andrew.emil@gmail.com>
2017-10-20 13:49:55 -0400

    Bug fix for floor problem

---
+++ Assets/Scripts/Controllers/FloorColliderController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorColliderController : MonoBehaviour {
+
+	// Use this for initialization
+	private Orchestrator orchestrator;
+	void Start () {
+		orchestrator = GameObject.Find ("Orchestrator").GetComponent<Orchestrator> ();
+	}
+	
+	// Update is called once per frame
+		
+	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Trigger with Floor");
+		if (other.name == "Player") {
+			orchestrator.TargetDied ();
+		}
+	}
+}
