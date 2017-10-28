8c3f077da155d6b6495061c7b33c84d5bbdb0064
ace <andrew.emil@gmail.com>
2017-10-28 13:10:59 -0400

    Started work on new antechamber

---
+++ Assets/Scripts/Controllers/RaceButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RaceButtonController : MonoBehaviour {
+
+	public int raceId;
+	private Level level;
+	private Button button;
+	private Text buttonText;
+	// Use this for initialization
+	void Start () {
+		button = GetComponentInChildren<Button> ();
+		buttonText = GetComponentInChildren<Text> ();
+		level = LevelProvider.GetLevel (raceId);
+
+		buttonText.text = level.name;
+		if (level.locked) {
+			LockLevel ();
+		}
+	}
+
+	private void LockLevel() {
+		button.interactable = false;
+	}
+
+	public void OnClick() {
+		SceneManager.LoadScene (level.name);
+	}
+}
