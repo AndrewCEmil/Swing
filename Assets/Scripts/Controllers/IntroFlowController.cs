7b75e10bfd14a14c51a050ba1e440254ace1db55
ace <andrew.emil@gmail.com>
2017-10-28 11:59:49 -0400

    Added intro flow, moving on to second intro level

---
+++ Assets/Scripts/Controllers/IntroFlowController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public enum IntroMode
+{
+	Basic,
+	Advanced
+}
+
+public class IntroFlowController : MonoBehaviour {
+
+	// Use this for initialization
+	public Text textField;
+	private IntroMode mode;
+	private int panelIndex;
+	private int clickFrameCountdown;
+	private bool isHighlighted;
+	void Start () {
+		clickFrameCountdown = 0;
+		string sceneName = SceneManager.GetActiveScene ().name;
+		if (sceneName == "Race6") {
+			mode = IntroMode.Basic;
+		} else {
+			mode = IntroMode.Advanced;
+		}
+		FillPanelText ();
+		isHighlighted = false;
+	}
+
+	void Update() {
+		if (clickFrameCountdown > 0) {
+			clickFrameCountdown--;
+		}
+	}
+
+	public void PointerEnter() {
+		isHighlighted = true;
+	}
+
+	public void PointerExit() {
+		isHighlighted = false;
+	}
+
+	public bool RecentlyClicked() {
+		return clickFrameCountdown > 0;
+	}
+
+	public bool IsHighlighted() {
+		return isHighlighted;
+	}
+
+	private void FillPanelText() {
+		string panelText = GetPanelText ();
+		textField.text = panelText;
+	}
+
+	private string GetPanelText() {
+		if (mode == IntroMode.Basic) {
+			return GetBasicPanelText();
+		}
+		return "";
+	}
+
+	private string GetBasicPanelText() {
+		switch(panelIndex) {
+		case 0:
+			return "Welcome!  First off, note the menu directly above you.  If you ever want exit play, just click Back up there.";
+		case 2:
+			return "The goal of each level is to hit the target.  The target is the Red Square in front of you.";
+		case 3:
+			return "To hit the target you will have to swing through the level by connecting to anchors.  The diamond in front of you is an anchor.";
+		case 4:
+			return "Connect to the Diamond by pointing your controller at it and clicking the touchpad.";
+		default: 
+			return "Connect to the Diamond by pointing your controller at it and clicking the touchpad.";
+		}
+	}
+
+	private string GetIntermediatePanelText() {
+		switch (panelIndex) {
+		default:
+			return "";
+
+		}
+	}
+
+	public void NextButtonClicked() {
+		panelIndex += 1;
+		clickFrameCountdown = 4;
+		FillPanelText ();
+	}
+}
--- Assets/Scripts/Orchestrator.cs
+++ Assets/Scripts/Orchestrator.cs
@@ -9,35 +9,44 @@ public class Orchestrator : MonoBehaviour {
 	private GameObject player;
 	private Rigidbody playerRb;
 	private Grappler grappler;
-	private Vector3 startForce;
 	private LevelController levelController;
 	private TimerController timerController;
 	private LeaderboardController leaderboardController;
 	private SoundEffectController sfxController;
 	private GameObject platform;
+	private GameObject introCanvas;
+	private IntroFlowController introFlowController;
 	void Start () {
 		player = GameObject.Find ("Player");
 		playerRb = player.GetComponent<Rigidbody> ();
 		grappler = player.GetComponent<Grappler> ();
-		startForce = new Vector3 (2000, 0, 0);
 		levelController = GameObject.Find ("LevelObj").GetComponent<LevelController> ();
 		timerController = GameObject.Find ("Timer").GetComponent<TimerController> ();
 		leaderboardController = GameObject.Find ("Leaderboard").GetComponent<LeaderboardController> ();
 		sfxController = GameObject.Find ("SoundEffectController").GetComponent<SoundEffectController> ();
+		introCanvas = GameObject.Find ("IntroCanvas");
+		if (introCanvas != null) {
+			introFlowController = introCanvas.GetComponent<IntroFlowController> ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GvrController.AppButtonUp) {
+		if (GvrControllerInput.AppButtonUp) {
 			HandleAppButton ();
 		}
 
-		if (GvrController.ClickButtonDown) {
+		if (GvrControllerInput.ClickButtonDown) {
 			PointerClicked ();
 		}
 	}
 
 	private void PointerClicked() {
+		if (introCanvas != null) {
+			if (introFlowController.IsHighlighted() || introFlowController.RecentlyClicked ()) {
+				return;
+			}
+		}
 		//TODO another sound
 		if (!timerController.RaceStarted ()) {
 			timerController.StartRace ();
