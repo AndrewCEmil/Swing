1058e791a46e2c1b3dc2a86a2a39088a5dcf9ee0
ace <andrew.emil@gmail.com>
2017-11-19 14:05:29 -0500

    Styling for antechamber

--- Assets/Scripts/Controllers/RaceButtonController.cs
+++ Assets/Scripts/Controllers/RaceButtonController.cs
@@ -46,7 +46,7 @@ public class RaceButtonController : MonoBehaviour {
 	//TODO maybe do this on a delay?
 	private void NewlyUnlocked() {
 		ColorBlock colorBlock = button.colors;
-		colorBlock.normalColor = new Color (234f/255f, 141f/255f, 219f/255f, .33f);
+		colorBlock.normalColor = new Color (234f/255f, 141f/255f, 219f/255f, .66f);
 		colorBlock.highlightedColor = new Color (234f/255f, 141f/255f, 219f/255f, .90f);
 		button.colors = colorBlock;
 		GameObject.Find ("SoundEffectController").GetComponent<SoundEffectController> ().PlayUnlock ();
@@ -55,10 +55,10 @@ public class RaceButtonController : MonoBehaviour {
 	private void SetColor(bool completed) {
 		ColorBlock colorBlock = button.colors;
 		if (completed) {
-			colorBlock.normalColor = new Color (0f/255f, 116f/255f, 39f/255f, 0.33f);
+			colorBlock.normalColor = new Color (0f/255f, 116f/255f, 39f/255f, 0.66f);
 			colorBlock.highlightedColor = new Color (0f/255f, 116f/255f, 39f/255f, 0.90f);
 		} else {
-			colorBlock.normalColor = new Color (44f/255f, 174f/255f, 186f/255f, 0.33f);
+			colorBlock.normalColor = new Color (44f/255f, 174f/255f, 186f/255f, 0.66f);
 			colorBlock.highlightedColor = new Color (44f/255f, 174f/255f, 186f/255f, 0.90f);
 		}
 		button.colors = colorBlock;
