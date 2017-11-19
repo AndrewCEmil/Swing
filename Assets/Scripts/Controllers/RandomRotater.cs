09f4ff4404a29751b726e18474774f18a95e7dbc
ace <andrew.emil@gmail.com>
2017-11-19 12:53:23 -0500

    Added support for art objects, but it doesnt look great honestly

---
+++ Assets/Scripts/Controllers/RandomRotater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddTorque (Random.onUnitSphere * 100f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
