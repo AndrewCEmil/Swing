a14af9013eba8c323b3788f837cd7aedb8f24ad4
ace <andrew.emil@gmail.com>
2017-12-01 20:50:14 -0500

    Bug fix for line particle system, moved intro canvas a bit

---
+++ Assets/Scripts/Controllers/LineParticleSystemController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineParticleSystemController : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		offset = new Vector3 (0, .3f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
