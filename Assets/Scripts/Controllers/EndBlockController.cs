daf007900d4b4fe7c6ad48ad909fa9cfcd3e7481
ace <andrew.emil@gmail.com>
2017-12-03 18:48:16 -0500

    Support for prefabs on endblocks

---
+++ Assets/Scripts/Controllers/EndBlockController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBlockController : MonoBehaviour {
+
+	GameObject player;
+	Rigidbody playerRb;
+	Grappler grappler;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		playerRb = player.GetComponent<Rigidbody> ();
+		grappler = player.GetComponent<Grappler> ();
+	}
+
+	public void ResetPlayerClicked() {
+		player.transform.position = transform.position - new Vector3 (0, 20, 0);
+		playerRb.velocity = Vector3.zero;
+		playerRb.angularVelocity = Vector3.zero;
+		grappler.Attach (gameObject);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
