bb70e68b51aa9a6316a8b68eaaa438cb1a72e69b
ace <andrew.emil@gmail.com>
2018-01-28 15:50:13 -0500

    Support for quitting properly

---
+++ Assets/Scripts/Controllers/QuitController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// Ensures correct app and scene setup.
+public class QuitController : MonoBehaviour {
+	void Start() {
+		Input.backButtonLeavesApp = true;
+	}
+
+	void Update() {
+		// Exit when (X) is tapped.
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
+
